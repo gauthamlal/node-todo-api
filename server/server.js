01fdb1e56573ff2cf78b86abe91d5ae34534cfff
Gautham Lal <gautham1995@gmail.com>
2018-10-17 21:09:38 +0530

    Added tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -103,7 +103,7 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then( token => {
-    res.header('x-auth', token).send({user});
+    res.header('x-auth', token).send(user);
   }).catch( e => {
     res.status(400).send(e);
   });
---
+++ server/tests/seed/seed.js
@@ -0,0 +1,50 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
+}];
+
+const users = [{
+  _id: userOneId,
+  email: 'something@somewhere.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'someguy@somewhere.com',
+  password: 'userTwoPass'
+}];
+
+const populateTodos = (done) => {
+  // this.timeout(10000);
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.deleteMany({}).then(() => {
+    let userOne = new User(users[0]).save();
+    let userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
